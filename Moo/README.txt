1a2cf1d043e1eca0f1505840d9e9ec7f945c4239
Alexandre Bourget <alex@bourget.cc>
2012-10-13 23:10:33 -0400

    First pyramid setup, with H5BP almost set.

---
+++ Moo/README.txt
@@ -0,0 +1,1 @@
+Moo README
---
+++ Moo/moo/views.py
@@ -0,0 +1,5 @@
+from pyramid.view import view_config
+
+@view_config(route_name='home', renderer='templates/index.html')
+def my_view(request):
+    return {'project':'Moo'}
