6c0457243278a11bc47cc9b7c501b65f0f8a5c6c
Alexandre Bourget <alex@bourget.cc>
2012-10-13 23:30:11 -0400

    Pyramid is all set for the rest of the integration.
    This part should not take more than 3 minutes, hopefully.

--- Moo/moo/__init__.py
+++ Moo/moo/__init__.py
@@ -4,6 +4,7 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+    config.add_renderer('.html', "pyramid.mako_templating.renderer_factory")
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
