c6bfaac19035ce6a04c9193683ab913ebfe40e2a
Alexandre Bourget <alex@bourget.cc>
2012-10-14 00:04:31 -0400

    Simple data-binding example closes
    IMPROVEMENTS: don't use ng-cloak if possible.. seems like a weakness and
    we don't want to create a bad feeling up front, and we don't want to take
    the time to explain at this time.

--- Moo/moo/static/js/main.js
+++ Moo/moo/static/js/main.js
@@ -1,1 +1,6 @@
-
+angular.module('moo', [])
+  // http://docs.angularjs.org/guide/di, see section "Inline Annotation" for the best way to declare these things.
+  .controller("MooCtrl", function($scope) {
+    $scope.who = 'Bobby';
+    console.log("We're up!");
+  })
